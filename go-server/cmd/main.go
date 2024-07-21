e6f259e78064ee4bd9eebc854b7db0ab3446ede3
Gabriel Cristofani <gabrielcristofanioliveira@gmail.com>
2024-07-20 22:24:52 -0300

    cotacao xy

--- go-server/cmd/main.go
+++ go-server/cmd/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	controller "github.com/gabscristofani/go-server/internal/controller/action"
+)
+
+func main() {
+	controller.CriarTabela()
+	http.HandleFunc("/cotacao", controller.Handle)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
