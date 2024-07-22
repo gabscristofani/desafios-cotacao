927881ab2f59bfd1899e448a1b8b51e315e47bd4
Gabriel Cristofani <gabrielcristofanioliveira@gmail.com>
2024-07-22 11:26:39 -0300

    subindo main

--- go-client/cmd/main.go
+++ go-client/cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	controller "github.com/gabscristofani/go-client/internal/controller/action"
+)
+
+func main() {
+	http.HandleFunc("/cotacao", controller.Handle)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
